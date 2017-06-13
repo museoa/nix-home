e1581276f67755ae537f44722765f3a3e492003f
Adam Scott <ascott.ca@gmail.com>
2017-06-12 20:23:23 -0400

    Try this commit.

--- lib/nixhome/default.nix
+++ lib/nixhome/default.nix
@@ -1,16 +1,16 @@
-{ stdenv, pkgs, lib }:
+{ stdenv, pkgs }:
 
 let
-  writeFiles = ../writeFiles.py;
   mkHome = { user, files, environment ? {} }: let
+  writeFiles = ./../writeFiles.py;
     src = pkgs.writeText "${user}-nix-home.json" (builtins.toJSON {
        inherit files;
+       inherit environment;
     });
-
     in stdenv.mkDerivation {
       name = "${user}-nix-home";
       inherit src;
-      inherit environment;
+
       builder = pkgs.writeText "builder.sh" ''
         #!/bin/sh
 
@@ -19,7 +19,7 @@ let
         mkdir -p $out
         ${pkgs.python}/bin/python ${writeFiles} "$src" "$out"
       '';
-  };
+  } ++ environment;
 in
 {
   inherit mkHome;
